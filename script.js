f2d0bf6334b38137aaa627a08dc1f271594cb224
mamukinto <43648003+mamukinto@users.noreply.github.com>
2019-09-14 18:18:04 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function getRandomInt(max) {
 function animation() {
 	first.style.animation = "shake 0.82s cubic-bezier(.36,.07,.19,.97) both";
 	second.style.animation = "shake 0.82s cubic-bezier(.36,.07,.19,.97) both";
-	first.style.marginLeft = getRandomInt(100) + "px";
-	first.style.marginBottom = getRandomInt(100) + "px";
+	first.style.marginLeft = getRandomInt(300) + "px";
+	first.style.marginTop = getRandomInt(300) + "px";
  setTimeout(function() {
  	first.style.animation = "0";
 	second.style.animation = "0";
@@ -17,13 +17,24 @@ function animation() {
 }
 
 function roll() {
-	first.style.backgroundImage = "url(" + getRandomInt(6) +".png)";
-	second.style.backgroundImage = "url(" + getRandomInt(6) +".png)";
+	var rand1 = getRandomInt(6);
+	var rand2 = getRandomInt(6);
+	if (rand1!=rand2) {
+		first.style.backgroundImage = "url(" + rand1 +".png)";
+		second.style.backgroundImage = "url(" + rand2 +".png)";
+}
+	else if (rand1 === rand2) {
+		first.style.backgroundImage = "url(" + rand1 +"b.png)";
+		second.style.backgroundImage = "url(" + rand2+"b.png)";
+		spin(1);
+		spin(2);
+	}
 }
 function animatq() {
 	animation();
 	roll();
 }
+
 document.addEventListener("keydown", event => {
   if (event.isComposing || event.keyCode === 32) {
     animatq();
@@ -32,18 +43,17 @@ document.addEventListener("keydown", event => {
 });
 
 
-setInterval(function() {
-	var lol = Math.floor(Math.random() * Math.floor(2) + 1)
-	if (lol>1) {
+function spin(which) {
+	if (which===2) {
 		second.classList.add("lol");
 		setTimeout(function() {
 			second.classList.remove("lol");
 		}, 2000);
 	}
-	else if (lol<2) {
+	else if (which===1) {
 	first.classList.add("lol");
 	setTimeout(function() {
 			first.classList.remove("lol");
 		}, 2000);
 }
-}, getRandomInt(8000)+5000)
+}
